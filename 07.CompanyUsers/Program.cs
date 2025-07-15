2be5677d1568fea953867b6f3fe633d9dc935a0d
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-15 19:55:53 +0300

    Solution 7

---
+++ 07.CompanyUsers/Program.cs
@@ -0,0 +1,56 @@
+﻿/// <summary>
+///  07.CompanyUsers
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main()
+        {        
+            List<string> def = [.. Console.ReadLine()!.Split("->", StringSplitOptions.TrimEntries)];
+
+            Dictionary<string, List<string>> companies = [];
+
+            while (!def[0].Equals("End"))
+            {
+                string companyName = def[0];
+                string workerId = string.Empty;
+
+                if (def.Count > 1)
+                {
+                    workerId = def[1];
+                }
+
+#pragma warning disable CA1854
+                if (companies.ContainsKey(companyName))
+                {
+                    bool isSameId = companies[companyName].Contains(workerId);
+                    if (!isSameId)
+                    {
+                        companies[companyName].Add(workerId);                       
+                    }
+                }
+#pragma warning restore CA1854 
+                else
+                {
+#pragma warning disable IDE0028 
+                    companies.Add(companyName, new List<string>() { workerId });
+#pragma warning restore IDE0028 
+                }
+
+                def = [.. Console.ReadLine()!.Split("->", StringSplitOptions.TrimEntries)];
+            }
+
+            foreach ((string company, List<string> workers) in companies)
+            {
+                Console.WriteLine(company);
+                workers.ForEach(workerNumber => Console.WriteLine($"-- {workerNumber}"));
+            }
+        }
+    }
+}
